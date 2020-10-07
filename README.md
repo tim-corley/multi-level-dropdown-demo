74c9ecd39ae5ccc54f6602356a24a79e6b064d67
Tim Corley <tcorley215@gmail.com>
2020-10-07 09:26:13 -0400

    adding menu levles & applying dynamic height calc

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
-# Barebones React App Starter Kit
+# Dropdown Menu
 
-### Leverages Webpack & Tailwind CSS
-
-```
-$ git clone
-$ npm install
-$ npm run start
-```
-
-To create tunnel to expose public URL (while dev server is up - in this case at post 8080):
-
-```
-./ngrok http 8080 -host-header="localhost:8080"
-```
-
-TODO:
-
-- [ ] add configuration for jest
-- [x] update index.js
+[Tutorial](https://www.youtube.com/watch?v=IF6k0uZuypA)
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import { ArrowIcon } from './icons/arrow.svg';
 import { BellIcon } from './icons/bell.svg';
 import { BoltIcon } from './icons/bolt.svg';
@@ -17,6 +18,9 @@ const App = () => {
       <NavItem
         icon={<MessengerIcon src={require('./icons/messenger.svg')} />}
       />
+      <NavItem icon={<CaretIcon src={require('./icons/caret.svg')} />}>
+        <DropdownMenu />
+      </NavItem>
     </Navbar>
   );
 };
@@ -29,13 +33,128 @@ const Navbar = ({ children }) => {
   );
 };
 
-const NavItem = ({ icon }) => {
+const NavItem = ({ icon, children }) => {
+  const [open, setOpen] = useState(false);
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {icon}
       </a>
+      {open && children}
     </li>
+  );
+};
+
+const DropdownMenu = () => {
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+  }, []);
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  const DropdownItem = ({ children, leftIcon, rightIcon, goToMenu }) => {
+    return (
+      <a
+        href="#"
+        className="menu-item"
+        onClick={() => goToMenu && setActiveMenu(goToMenu)}
+      >
+        {leftIcon && <span className="icon-button">{leftIcon}</span>}
+        {children}
+        {rightIcon && <span className="icon-button">{rightIcon}</span>}
+      </a>
+    );
+  };
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === 'main'}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-primary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            goToMenu="settings"
+            leftIcon={<CogIcon src={require('./icons/cog.svg')} />}
+          >
+            Settings
+          </DropdownItem>
+          <DropdownItem goToMenu="animals" leftIcon="🦧">
+            Animals
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'settings'}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-secondary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem
+            goToMenu="main"
+            leftIcon={<ArrowIcon src={require('./icons/arrow.svg')} />}
+          >
+            <h2>My Skills</h2>
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={<BoltIcon src={require('./icons/bolt.svg')} />}
+          >
+            HTML
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={<BoltIcon src={require('./icons/bolt.svg')} />}
+          >
+            CSS
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={<BoltIcon src={require('./icons/bolt.svg')} />}
+          >
+            JavaScript
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={<BoltIcon src={require('./icons/bolt.svg')} />}
+          >
+            Awesomeness!
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={activeMenu === 'animals'}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          {' '}
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+            <h2>Animals</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
+          <DropdownItem leftIcon="🐸">Frog</DropdownItem>
+          <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
+          <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
+          <DropdownItem leftIcon="🛠">Test</DropdownItem>
+          <DropdownItem leftIcon="🛠">Test</DropdownItem>
+          <DropdownItem leftIcon="🛠">Test</DropdownItem>
+          <DropdownItem leftIcon="🛠">Test</DropdownItem>
+          <DropdownItem leftIcon="🛠">Test</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
   );
 };
 
